dac11466f7b39f9d8a522932fffd9c341e7495cb
reskinder <102713743+reskinder@users.noreply.github.com>
2022-07-15 14:54:19 -0700

    Update spark_queries.py

--- Ecommerce Program/spark_queries.py
+++ Ecommerce Program/spark_queries.py
@@ -42,23 +42,37 @@ toydf = toydf.withColumn("product_id", col("product_id").cast("int"))
 
 # # MARKETING Q1: What is the top selling category of items? Per Country?
 print("QUESTION 1: What is the top selling category of items? Per Country?")
-toydf.select("country", "product_category", "qty").where(toydf.payment_txn_success=="Y")\
+marketQ1 = toydf.withColumn("year", year(toydf.datetime))
+# Year 2020
+print("Year 2020:")
+marketQ1.select("country", "product_category", "qty").where(marketQ1.payment_txn_success=="Y").where(marketQ1.year==2020)\
+.groupBy("country", "product_category").agg({"qty":"sum"}).orderBy(col("country").desc(), col("sum(qty)").desc(), col("product_category").desc()).show()
+# Year 2021
+print("Year 2021:")
+marketQ1.select("country", "product_category", "qty").where(marketQ1.payment_txn_success=="Y").where(marketQ1.year==2021)\
+.groupBy("country", "product_category").agg({"qty":"sum"}).orderBy(col("country").desc(), col("sum(qty)").desc(), col("product_category").desc()).show()
+# Year 2022
+print("Year 2022:")
+marketQ1.select("country", "product_category", "qty").where(marketQ1.payment_txn_success=="Y").where(marketQ1.year==2022)\
 .groupBy("country", "product_category").agg({"qty":"sum"}).orderBy(col("country").desc(), col("sum(qty)").desc(), col("product_category").desc()).show()
 
 # MARKETING Q2: How does the popularity of products change throughout the year? Per Country?
 print("QUESTION 2: How does the popularity of products change throughout the year? Per Country?")
 marketQ2 = toydf.withColumn("quarter", quarter(toydf.datetime)).withColumn("year", year(toydf.datetime))
 # Year 2020
+print("Year 2020:")
 marketQ2.select("country", "product_name", "qty", "quarter", "year").where(marketQ2.payment_txn_success=="Y").where(marketQ2.year==2020)\
-.groupBy("year", "quarter", "country", "product_name").agg({"qty":"sum"})\
+.groupBy("quarter", "country", "product_name").agg({"qty":"sum"})\
 .orderBy(col("quarter").asc(), col("sum(qty)").desc()).show(truncate=False)
 # Year 2021
+print("Year 2021:")
 marketQ2.select("country", "product_name", "qty", "quarter", "year").where(marketQ2.payment_txn_success=="Y").where(marketQ2.year==2021)\
-.groupBy("year", "quarter", "country", "product_name").agg({"qty":"sum"})\
+.groupBy("quarter", "country", "product_name").agg({"qty":"sum"})\
 .orderBy(col("quarter").asc(), col("sum(qty)").desc()).show(truncate=False)
 # Year 2022
+print("Year 2022:")
 marketQ2.select("country", "product_name", "qty", "quarter", "year").where(marketQ2.payment_txn_success=="Y").where(marketQ2.year==2022)\
-.groupBy("year", "country", "product_name", "quarter").agg({"qty":"sum"})\
+.groupBy("quarter", "country", "product_name").agg({"qty":"sum"})\
 .orderBy(col("quarter").asc(), col("sum(qty)").desc()).show(truncate=False)
 
 # MARKETING Q3: Which locations see the highest traffic of sales?
@@ -79,19 +93,54 @@ marketQ3.select("country","qty").where(marketQ3.payment_txn_success=="Y").where(
 
 # MARKETING Q4: Which website had the most sales?
 print("QUESTION 4: Which website had the most sales?")
-toydf.select("ecommerce_website_name","qty",).where(toydf.payment_txn_success=="Y")\
+marketQ4 = toydf.withColumn("year", year(toydf.datetime))
+# Year 2020
+print("Year 2020:")
+marketQ4.select("ecommerce_website_name","qty",).where(marketQ4.payment_txn_success=="Y").where(marketQ4.year==2020)\
+.groupBy("ecommerce_website_name").agg({"qty":"sum"}).orderBy(col("sum(qty)").desc()).show(1, truncate=False)
+# Year 2021
+print("Year 2021:")
+marketQ4.select("ecommerce_website_name","qty",).where(marketQ4.payment_txn_success=="Y").where(marketQ4.year==2021)\
+.groupBy("ecommerce_website_name").agg({"qty":"sum"}).orderBy(col("sum(qty)").desc()).show(1, truncate=False)
+# Year 2022
+print("Year 2022:")
+marketQ4.select("ecommerce_website_name","qty",).where(marketQ4.payment_txn_success=="Y").where(marketQ4.year==2022)\
 .groupBy("ecommerce_website_name").agg({"qty":"sum"}).orderBy(col("sum(qty)").desc()).show(1, truncate=False)
 
 # MARKETING Q5: Which the most common form of payment failure?
 print("QUESTION 5: Which the most common form of payment failure?")
-toydf.select("failure_reason").where(toydf.payment_txn_success=="N")\
+marketQ5 = toydf.withColumn("year", year(toydf.datetime))
+# Year 2020
+print("Year 2020:")
+marketQ5.select("failure_reason").where(marketQ5.payment_txn_success=="N").where(marketQ5.year==2020)\
+.groupBy("failure_reason").agg({"failure_reason":"count"})\
+.orderBy(col("count(failure_reason)").desc()).show(1)
+# Year 2021
+print("Year 2021:")
+marketQ5.select("failure_reason").where(marketQ5.payment_txn_success=="N").where(marketQ5.year==2021)\
+.groupBy("failure_reason").agg({"failure_reason":"count"})\
+.orderBy(col("count(failure_reason)").desc()).show(1)
+# Year 2022
+print("Year 2022:")
+marketQ5.select("failure_reason").where(marketQ5.payment_txn_success=="N").where(marketQ5.year==2022)\
 .groupBy("failure_reason").agg({"failure_reason":"count"})\
 .orderBy(col("count(failure_reason)").desc()).show(1)
 
 # MARKETING Q6: Which is the most common form of payment method?
 print("QUESTION 6: Which is the most common form of payment method?")
-toydf.select("payment_type","qty",).where(toydf.payment_txn_success=="Y")\
-.groupBy("payment_type").agg({"qty":"sum"}).orderBy(col("sum(qty)").desc()).show()
+marketQ6 = toydf.withColumn("year", year(toydf.datetime))
+# Year 2020
+print("Year 2020:")
+marketQ6.select("payment_type").where(marketQ6.payment_txn_success=="Y").where(marketQ6.year==2020)\
+.groupBy("payment_type").agg({"payment_type":"count"}).orderBy(col("count(payment_type)").desc()).show()
+# Year 2021
+print("Year 2021:")
+marketQ6.select("payment_type","qty",).where(marketQ6.payment_txn_success=="Y").where(marketQ6.year==2021)\
+.groupBy("payment_type").agg({"payment_type":"count"}).orderBy(col("count(payment_type)").desc()).show()
+# Year 2022
+print("Year 2022:")
+marketQ6.select("payment_type","qty",).where(marketQ6.payment_txn_success=="Y").where(marketQ6.year==2022)\
+.groupBy("payment_type").agg({"payment_type":"count"}).orderBy(col("count(payment_type)").desc()).show()
 
 # MARKETING Q7: What times have the highest traffic of sales? Per Country?
 print("QUESTION 7: What times have the highest traffic of sales? Per Country?")
