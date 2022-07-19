c92c1b7fa389ec213c3bdf47c6f3326f0dfac608
reskinder <102713743+reskinder@users.noreply.github.com>
2022-07-19 14:35:05 -0700

    Update spark_queries.py; added aliases

--- Ecommerce Program/spark_queries.py
+++ Ecommerce Program/spark_queries.py
@@ -10,7 +10,7 @@ findspark.init()
 from pyspark import SparkContext, sql, SparkConf
 from pyspark.sql import SparkSession, Row, HiveContext, SQLContext
 import pyspark
-from pyspark.sql.functions import col, quarter, year, hour
+from pyspark.sql.functions import col, quarter, year, hour, sum, count
 import os
 import sys
 
@@ -52,15 +52,18 @@ def marketQueries(menuChoice):
         # Year 2020
         print("Year 2020:")
         marketQ1.select("country", "product_category", "qty").where(marketQ1.payment_txn_success=="Y").where(marketQ1.year==2020)\
-        .groupBy("country", "product_category").agg({"qty":"sum"}).orderBy(col("country").desc(), col("sum(qty)").desc(), col("product_category").desc()).show()
+        .groupBy(marketQ1.country.alias("Country"), marketQ1.product_category.alias("Product Category")).agg(sum(marketQ1.qty).alias("Quantity"))\
+        .orderBy(col("Country").desc(), col("Quantity").desc(), col("Product Category").desc()).show()
         # Year 2021
         print("Year 2021:")
         marketQ1.select("country", "product_category", "qty").where(marketQ1.payment_txn_success=="Y").where(marketQ1.year==2021)\
-        .groupBy("country", "product_category").agg({"qty":"sum"}).orderBy(col("country").desc(), col("sum(qty)").desc(), col("product_category").desc()).show()
+        .groupBy(marketQ1.country.alias("Country"), marketQ1.product_category.alias("Product Category")).agg(sum(marketQ1.qty).alias("Quantity"))\
+        .orderBy(col("Country").desc(), col("Quantity").desc(), col("Product Category").desc()).show()
         # Year 2022
         print("Year 2022:")
         marketQ1.select("country", "product_category", "qty").where(marketQ1.payment_txn_success=="Y").where(marketQ1.year==2022)\
-        .groupBy("country", "product_category").agg({"qty":"sum"}).orderBy(col("country").desc(), col("sum(qty)").desc(), col("product_category").desc()).show()
+        .groupBy(marketQ1.country.alias("Country"), marketQ1.product_category.alias("Product Category")).agg(sum(marketQ1.qty).alias("Quantity"))\
+        .orderBy(col("Country").desc(), col("Quantity").desc(), col("Product Category").desc()).show()
     elif (menuChoice == 2):
         # MARKETING Q2: How does the popularity of products change throughout the year? Per Country?
         print("\nQUESTION 2: How does the popularity of products change throughout the year? Per Country?")
@@ -68,18 +71,18 @@ def marketQueries(menuChoice):
         # Year 2020
         print("Year 2020:")
         marketQ2.select("country", "product_name", "qty", "quarter", "year").where(marketQ2.payment_txn_success=="Y").where(marketQ2.year==2020)\
-        .groupBy("quarter", "country", "product_name").agg({"qty":"sum"})\
-        .orderBy(col("quarter").asc(), col("sum(qty)").desc()).show(truncate=False)
+        .groupBy(marketQ2.quarter.alias("Year Quarter"), marketQ2.country.alias("Country"), marketQ2.product_name.alias("Product Name"))\
+        .agg(sum(marketQ2.qty).alias("Quantity")).orderBy(col("Year Quarter").asc(), col("Quantity").desc()).show(truncate=False)
         # Year 2021
         print("Year 2021:")
         marketQ2.select("country", "product_name", "qty", "quarter", "year").where(marketQ2.payment_txn_success=="Y").where(marketQ2.year==2021)\
-        .groupBy("quarter", "country", "product_name").agg({"qty":"sum"})\
-        .orderBy(col("quarter").asc(), col("sum(qty)").desc()).show(truncate=False)
+        .groupBy(marketQ2.quarter.alias("Year Quarter"), marketQ2.country.alias("Country"), marketQ2.product_name.alias("Product Name"))\
+        .agg(sum(marketQ2.qty).alias("Quantity")).orderBy(col("Year Quarter").asc(), col("Quantity").desc()).show(truncate=False)
         # Year 2022
         print("Year 2022:")
         marketQ2.select("country", "product_name", "qty", "quarter", "year").where(marketQ2.payment_txn_success=="Y").where(marketQ2.year==2022)\
-        .groupBy("quarter", "country", "product_name").agg({"qty":"sum"})\
-        .orderBy(col("quarter").asc(), col("sum(qty)").desc()).show(truncate=False)
+        .groupBy(marketQ2.quarter.alias("Year Quarter"), marketQ2.country.alias("Country"), marketQ2.product_name.alias("Product Name"))\
+        .agg(sum(marketQ2.qty).alias("Quantity")).orderBy(col("Year Quarter").asc(), col("Quantity").desc()).show(truncate=False)
     elif (menuChoice == 3):
         # MARKETING Q3: Which locations see the highest traffic of sales?
         print("\nQUESTION 3: Which locations see the highest traffic of sales?")
@@ -87,50 +90,47 @@ def marketQueries(menuChoice):
         # Year 2020
         print("Year 2020:")
         marketQ3.select("country","qty").where(marketQ3.payment_txn_success=="Y").where(marketQ3.year=="2020")\
-        .groupBy("country").agg({"qty":"sum"}).orderBy(col("sum(qty)").desc()).show(truncate=False)
+        .groupBy(marketQ3.country.alias("Country")).agg(sum(marketQ3.qty).alias("Quantity")).orderBy(col("Quantity").desc()).show(truncate=False)
         # Year 2021
         print("Year 2021:")
         marketQ3.select("country","qty").where(marketQ3.payment_txn_success=="Y").where(marketQ3.year=="2021")\
-        .groupBy("country").agg({"qty":"sum"}).orderBy(col("sum(qty)").desc()).show(truncate=False)
+        .groupBy(marketQ3.country.alias("Country")).agg(sum(marketQ3.qty).alias("Quantity")).orderBy(col("Quantity").desc()).show(truncate=False)
         # Year 2022
         print("Year 2022:")
         marketQ3.select("country","qty").where(marketQ3.payment_txn_success=="Y").where(marketQ3.year=="2022")\
-        .groupBy("country").agg({"qty":"sum"}).orderBy(col("sum(qty)").desc()).show(truncate=False)
+        .groupBy(marketQ3.country.alias("Country")).agg(sum(marketQ3.qty).alias("Quantity")).orderBy(col("Quantity").desc()).show(truncate=False)
     elif (menuChoice == 4):
         # MARKETING Q4: Which website had the most sales?
         print("\nQUESTION 4: Which website had the most sales?")
         marketQ4 = toydf.withColumn("year", year(toydf.datetime))
         # Year 2020
         print("Year 2020:")
-        marketQ4.select("ecommerce_website_name","qty",).where(marketQ4.payment_txn_success=="Y").where(marketQ4.year==2020)\
-        .groupBy("ecommerce_website_name").agg({"qty":"sum"}).orderBy(col("sum(qty)").desc()).show(1, truncate=False)
+        marketQ4.select(marketQ4.ecommerce_website_name.alias("Ecommerce Website"),"qty",).where(marketQ4.payment_txn_success=="Y").where(marketQ4.year==2020)\
+        .groupBy("Ecommerce Website").agg(sum(marketQ4.qty).alias("Quantity")).orderBy(col("Quantity").desc()).show(1, truncate=False)
         # Year 2021
         print("Year 2021:")
-        marketQ4.select("ecommerce_website_name","qty",).where(marketQ4.payment_txn_success=="Y").where(marketQ4.year==2021)\
-        .groupBy("ecommerce_website_name").agg({"qty":"sum"}).orderBy(col("sum(qty)").desc()).show(1, truncate=False)
+        marketQ4.select(marketQ4.ecommerce_website_name.alias("Ecommerce Website"),"qty",).where(marketQ4.payment_txn_success=="Y").where(marketQ4.year==2021)\
+        .groupBy("Ecommerce Website").agg(sum(marketQ4.qty).alias("Quantity")).orderBy(col("Quantity").desc()).show(1, truncate=False)
         # Year 2022
         print("Year 2022:")
-        marketQ4.select("ecommerce_website_name","qty",).where(marketQ4.payment_txn_success=="Y").where(marketQ4.year==2022)\
-        .groupBy("ecommerce_website_name").agg({"qty":"sum"}).orderBy(col("sum(qty)").desc()).show(1, truncate=False)
+        marketQ4.select(marketQ4.ecommerce_website_name.alias("Ecommerce Website"),"qty",).where(marketQ4.payment_txn_success=="Y").where(marketQ4.year==2022)\
+        .groupBy("Ecommerce Website").agg(sum(marketQ4.qty).alias("Quantity")).orderBy(col("Quantity").desc()).show(1, truncate=False)
     elif (menuChoice == 5):
-        # MARKETING Q5: Which the most common form of payment failure?
-        print("\nQUESTION 5: Which the most common form of payment failure?")
+        # MARKETING Q5: Which is the most common form of payment failure?
+        print("\nQUESTION 5: Which is the most common form of payment failure?")
         marketQ5 = toydf.withColumn("year", year(toydf.datetime))
         # Year 2020
         print("Year 2020:")
         marketQ5.select("failure_reason").where(marketQ5.payment_txn_success=="N").where(marketQ5.year==2020)\
-        .groupBy("failure_reason").agg({"failure_reason":"count"})\
-        .orderBy(col("count(failure_reason)").desc()).show(1)
+        .groupBy(marketQ5.failure_reason.alias("Failure Reason")).agg(count(marketQ5.failure_reason).alias("Count")).orderBy(col("Count").desc()).show(1)
         # Year 2021
         print("Year 2021:")
         marketQ5.select("failure_reason").where(marketQ5.payment_txn_success=="N").where(marketQ5.year==2021)\
-        .groupBy("failure_reason").agg({"failure_reason":"count"})\
-        .orderBy(col("count(failure_reason)").desc()).show(1)
+        .groupBy(marketQ5.failure_reason.alias("Failure Reason")).agg(count(marketQ5.failure_reason).alias("Count")).orderBy(col("Count").desc()).show(1)
         # Year 2022
         print("Year 2022:")
         marketQ5.select("failure_reason").where(marketQ5.payment_txn_success=="N").where(marketQ5.year==2022)\
-        .groupBy("failure_reason").agg({"failure_reason":"count"})\
-        .orderBy(col("count(failure_reason)").desc()).show(1)
+        .groupBy(marketQ5.failure_reason.alias("Failure Reason")).agg(count(marketQ5.failure_reason).alias("Count")).orderBy(col("Count").desc()).show(1)
     elif (menuChoice == 6):
         # MARKETING Q6: Which is the most common form of payment method?
         print("\nQUESTION 6: Which is the most common form of payment method?")
@@ -138,15 +138,15 @@ def marketQueries(menuChoice):
         # Year 2020
         print("Year 2020:")
         marketQ6.select("payment_type").where(marketQ6.payment_txn_success=="Y").where(marketQ6.year==2020)\
-        .groupBy("payment_type").agg({"payment_type":"count"}).orderBy(col("count(payment_type)").desc()).show()
+        .groupBy(marketQ6.payment_type.alias("Payment Type")).agg(count(marketQ6.payment_type).alias("Count")).orderBy(col("Count").desc()).show()
         # Year 2021
         print("Year 2021:")
         marketQ6.select("payment_type","qty",).where(marketQ6.payment_txn_success=="Y").where(marketQ6.year==2021)\
-        .groupBy("payment_type").agg({"payment_type":"count"}).orderBy(col("count(payment_type)").desc()).show()
+        .groupBy(marketQ6.payment_type.alias("Payment Type")).agg(count(marketQ6.payment_type).alias("Count")).orderBy(col("Count").desc()).show()
         # Year 2022
         print("Year 2022:")
         marketQ6.select("payment_type","qty",).where(marketQ6.payment_txn_success=="Y").where(marketQ6.year==2022)\
-        .groupBy("payment_type").agg({"payment_type":"count"}).orderBy(col("count(payment_type)").desc()).show()
+        .groupBy(marketQ6.payment_type.alias("Payment Type")).agg(count(marketQ6.payment_type).alias("Count")).orderBy(col("Count").desc()).show()
     elif (menuChoice == 7):
         # MARKETING Q7: What times have the highest traffic of sales? Per Country?
         print("\nQUESTION 7: What times have the highest traffic of sales? Per Country?")
@@ -154,15 +154,18 @@ def marketQueries(menuChoice):
         # Year 2020
         print("Year 2020:")
         marketQ7.select("country", "hour", "qty").where(marketQ7.payment_txn_success=="Y").where(marketQ7.year==2020)\
-        .groupBy("country", "hour").agg({"qty":"sum"}).orderBy(col("country").desc(), col("sum(qty)").desc(), col("hour").asc()).show(50, truncate=False)
+        .groupBy(marketQ7.country.alias("Country"), marketQ7.hour.alias("Hour")).agg(sum(marketQ7.qty).alias("Quantity"))\
+        .orderBy(col("Country").desc(), col("Quantity").desc(), col("Hour").asc()).show(50, truncate=False)
         # Year 2021
         print("Year 2021:")
         marketQ7.select("country", "hour", "qty").where(marketQ7.payment_txn_success=="Y").where(marketQ7.year==2021)\
-        .groupBy("country", "hour").agg({"qty":"sum"}).orderBy(col("country").desc(), col("sum(qty)").desc(), col("hour").asc()).show(50, truncate=False)
+        .groupBy(marketQ7.country.alias("Country"), "hour").agg(sum(marketQ7.qty).alias("Quantity"))\
+        .orderBy(col("Country").desc(), col("Quantity").desc(), col("Hour").asc()).show(50, truncate=False)
         # Year 2022
         print("Year 2022:")
         marketQ7.select("country", "hour", "qty").where(marketQ7.payment_txn_success=="Y").where(marketQ7.year==2022)\
-        .groupBy("country", "hour").agg({"qty":"sum"}).orderBy(col("country").desc(), col("sum(qty)").desc(), col("hour").asc()).show(50, truncate=False)
+        .groupBy(marketQ7.country.alias("Country"), marketQ7.hour.alias("Hour")).agg(sum(marketQ7.qty).alias("Quantity"))\
+        .orderBy(col("Country").desc(), col("Quantity").desc(), col("Hour").asc()).show(50, truncate=False)
 
 # Set Up PySpark Environment Variables
 os.environ['PYSPARK_PYTHON'] = sys.executable
